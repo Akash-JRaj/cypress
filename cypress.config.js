0daab7e49f405a10b5ec4910bd546c51010e46c9
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-21 18:17:05 +0530

    Added mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
+    reporter: "mochawesome",
     pageLoadTimeout: 60000,
     defaultCommandTimeout: 8000,
     screenshotOnRunFailure: true,
