6744313e37e7299f2f5ddb3649a3c49745d05fff
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-21 13:32:24 +0530

    Initial config changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 8000,
+    screenshotOnRunFailure: true,
+    video: true,
+    trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
